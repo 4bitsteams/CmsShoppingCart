04cbbb5ed69d70bd278bbec990bf0b30dfc0b574
md.rubel <md.rubel@HOAP2406101764A>
2021-09-14 00:45:32 +0600

    Add Page Controller

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        [Area("Admin")]
+        public string Index()
+        {
+            return "Test";
+        }
+    }
+}
