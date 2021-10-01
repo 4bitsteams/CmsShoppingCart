4f053ab14ed981572b831b79e02d0ce4258b93bf
md.rubel <md.rubel@HOAP2406101764A>
2021-10-02 01:16:04 +0600

    Add Session and some configuration in Startup class

--- CmsShoppingCart/Startup.cs
+++ CmsShoppingCart/Startup.cs
@@ -1,7 +1,9 @@
 using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,8 +27,29 @@ namespace CmsShoppingCart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+            services.AddSession(options =>
+            {
+                //options.IdleTimeout = TimeSpan.FromSeconds(2)
+                //options.IdleTimeout = TimeSpan.FromDays(2)
+            });
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
             services.AddControllersWithViews();
+
             services.AddDbContext<CmsShoppingCartContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CmsShoppingCartContext")));
+
+            services.AddIdentity<AppUser, IdentityRole>(options => {
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            })
+              .AddEntityFrameworkStores<CmsShoppingCartContext>()
+                   
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +71,10 @@ namespace CmsShoppingCart
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
+
+            app.UseAuthentication();
 
 
             app.UseEndpoints(endpoints =>
